1d87b3e2f82d64fe495c3862e4b7b8edcb659d81
sangramabdar <abdarsangram@gmail.com>
2021-05-21 16:37:03 +0530

    completely updated setup

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ module.exports = {
     require("autoprefixer"),
     purgeCss({
       content: ["./src/***.tsx", "./public/index.html"],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
   ],
 };
