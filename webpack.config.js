0cfe69e7ea76b17fc7ff32dcb09a4bce5d5eae05
sangramabdar <abdarsangram2697@gmail.com>
2021-11-12 10:06:22 +0530

    declare.d.ts added

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,56 @@
-
 const path = require("path");
 
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 const config = {
-  mode: "development",
+    mode: "development",
 
-  entry: path.resolve(__dirname, "src", "index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
-
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: true,
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [{
+                test: /\.(ts|tsx|js|jsx)$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
             },
-          },
+
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                ],
+            },
         ],
-      },
+    },
+
+    devServer: {
+        open: true,
+        historyApiFallback: true,
+    },
+
+    devtool: "source-map",
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html"),
+        }),
+        new CleanWebpackPlugin(),
     ],
-  },
-
-  devServer: {
-    // watchContentBase: true,
-    // contentBase: path.resolve(__dirname, "dist"),
-    open: true,
-    historyApiFallback: true,
-  },
-
-  devtool: "source-map",
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new CleanWebpackPlugin(),
-  ],
 };
-
 
 module.exports = config;
--- webpack.prod.js
+++ webpack.prod.js
@@ -7,50 +7,49 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
-  mode: "production",
+    mode: "production",
 
-  entry: path.resolve(__dirname, "src", "index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
-
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: true,
+    module: {
+        rules: [{
+                test: /\.(ts|tsx|js|jsx)$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
             },
-          },
-        ],
-      },
-    ],
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "index.css",
-    }),
-  ],
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html"),
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "index.css",
+        }),
+    ],
 };
 
 module.exports = config;
